2ff24ae3d1d4dba14eaad9dfbab88d82808f3da5
Trivedi <trivecha@amazon.com>
2024-12-13 19:07:59 -0700

    updated protected route component

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 // import { jwtDecode } from 'jwt-decode';
 
-const ProtectedRoute = ({ element: Component, ...props }) => {
+const ProtectedRoute = ({ children, adminOnly = false }) => {
   const token = localStorage.getItem('auth_token'); // Check if token exists in localStorage
   const user = JSON.parse(localStorage.getItem('user'))
 
@@ -14,13 +14,12 @@ const ProtectedRoute = ({ element: Component, ...props }) => {
   const decodedToken = JSON.parse(atob(token.split('.')[1])); // Decode the JWT token
   const userRole = decodedToken?.role; // Extract role from decoded token
 
-  if (!user || user.role !== 'admin') {
-    alert('Not authorized user')
-    return <Navigate to="/" replace />;
+  if (adminOnly && (!user || user.role !== 'admin')) {
+    return <Navigate to="/" replace />;;
   }
 
   // If token exists, render the provided component with props
-  return <Outlet />;
+  return children;
 };
 
 export default ProtectedRoute;
